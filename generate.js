fdd4817761f69c157c892f8e93ba43da270bc4ef
Arturo <kelyhpyva@protonmail.com>
2022-10-15 20:05:19 +0300

    faq

--- generate.js
+++ generate.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 function generate() {
   const metadata = {
     image:
-      "https://gateway.pinata.cloud/ipfs/QmWtgaVtriEhLo6dLPLK2EJ2VjCgSFMD8WQZcxzXpxH9r8",
-    name: "Trace NFT Passio",
+      "https://gateway.pinata.cloud/ipfs/QmbJ8h6vNeova1SDbrJAiqQuMsCmGyuPHy7xuQ3y34TjSd",
+    name: "Trace NFT Pass HIDE",
     description:
       "A special Genesis NFT Pass collection, which will be distributed to holders who have bought or " +
       "received NFT Containers with OG NFT Cars inside. Genesis NFT Pass is a NFTs in metaverse Trace " +
@@ -17,7 +17,7 @@ function generate() {
   for (let i = 0; i <= 9999; i++) {
     fs.writeFile(
       `./upload/${i}`,
-      JSON.stringify({ ...metadata, ...{ name: `Trace NFT Passio #${i}` } }),
+      JSON.stringify({ ...metadata, ...{ name: `Trace NFT Pass HIDE #${i}` } }),
       function (err) {
         if (err) throw err;
         console.log(`Complete ${i}`);
