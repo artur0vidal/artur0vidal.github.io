d8726dfcc0d9dd40a49081b2721e8048a94c277c
Arturo <kelyhpyva@protonmail.com>
2022-10-17 02:04:43 +0300

    +

--- generate.js
+++ generate.js
@@ -3,20 +3,16 @@ const fs = require("fs");
 
 function generate() {
   const metadata = {
-    animation_url: "https://artur0vidal.github.io/image/qr.mp4",
-    name: "Trace Genesis NFT Pass",
-    description:
-      "A special Genesis NFT Pass collection, which will be distributed to TRACE holders " +
-      "who have bought or received NFT Containers with OG NFT Cars inside. Genesis NFT Pass " +
-      "will give privileges to its holders within the entire TRACE project and other " +
-      "mechanics added to the project.\n\nTracer, let’s explore the world! You are a part " +
-      "of OG TRACE fam.",
+    image: "https://artur0vidal.github.io/image/image.png",
+    animation_url: "https://artur0vidal.github.io/image/video.mp4",
+    name: "Test",
+    description: "Test description",
   };
 
   for (let i = 0; i <= 9999; i++) {
     fs.writeFile(
       join(__dirname, "item", i + ""),
-      JSON.stringify({ ...metadata, ...{ name: `Genesis NFT Pass #${i}` } }),
+      JSON.stringify({ ...metadata, ...{ name: `Test NFT Wow #${i}` } }),
       function (err) {
         if (err) throw err;
         console.log(`Complete ${i}`);
